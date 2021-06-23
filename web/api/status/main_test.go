83ad1cf65f985b105ad5bd901acb4b2a0fa263e8
Geno <geno+dev@fireorbit.de>
2021-06-23 13:39:57 +0200

    test(web/api/status): 100% LOC

--- web/api/status/main_test.go
+++ web/api/status/main_test.go
@@ -15,9 +15,18 @@ func TestAPIStatus(t *testing.T) {
 	assert.NotNil(s)
 
 	obj := Status{}
-	// GET - common name
+	// GET
 	s.Request(http.MethodGet, "/api/status", nil, http.StatusOK, &obj)
 	assert.Equal(VERSION, obj.Version)
 	assert.Equal(EXTRAS, obj.Extras)
 	assert.True(obj.Up)
+
+	UP = func() bool { return false }
+
+	obj = Status{}
+	// GET - failed status
+	s.Request(http.MethodGet, "/api/status", nil, http.StatusInternalServerError, &obj)
+	assert.Equal(VERSION, obj.Version)
+	assert.Equal(EXTRAS, obj.Extras)
+	assert.False(obj.Up)
 }
